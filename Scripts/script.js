83f74d51f41e58731da9c38560211e2bd031d1ce
BaiBax <p.r.worthey@gmail.com>
2022-11-30 12:02:16 -0500

    Update game to return winner from promt response

---
+++ Scripts/script.js
@@ -0,0 +1,36 @@
+const computerSelection = computerTurn();
+
+const playerSelection = playerTurn();
+
+playGame(computerSelection, playerSelection);
+
+function computerTurn(){
+    let x = Math.floor(Math.random() * 3);
+    if(x == 0){
+        return("rock");
+    }else if(x == 1){
+        return("paper");
+    }else{
+        return("scissors");
+    }
+}
+
+function playerTurn()
+{
+    return prompt("Enter your choice")
+
+}
+
+function playGame(computerSelection, playerSelection){
+    if(computerSelection == "rock" && playerSelection == "rock" ||  computerSelection == "paper" && playerSelection == "paper"  || computerSelection == "scissors" && playerSelection == "sciccors"){
+        alert("tie!");
+    }else if(computerSelection == "rock" && playerSelection == "paper" || computerSelection == "scissors" && playerSelection == "rock" || computerSelection == "paper" && playerSelection == "scissors"){
+        alert("player wins!");
+    }else if (computerSelection == "paper" && playerSelection == "rock" || computerSelection == "scissors" && playerSelection == "paper" || computerSelection == "rock" && playerSelection == "scissors"){
+        alert("computer wins!");
+    }else{
+        alert("Enter correct choice!");
+    }
+}
+
+console.log("Computer Selection \=", computerSelection, "Player Selection \=",playerSelection)
